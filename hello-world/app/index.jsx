6f19df0e94045d4e0392ccf26cd99f96ef02a2ca
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2024-08-20 09:22:09 +0000

    added index page

---
+++ hello-world/app/index.jsx
@@ -0,0 +1,20 @@
+import { View, Text, SafeAreaView } from 'react-native'
+
+const HelloWorld = () => {
+    
+    return (
+        <SafeAreaView style={{
+            backgroundColor: '#B99470'
+        }}>
+        <Text>Hello world</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HelloWorld
+
+//git commands 
+//git add .
+//git commit
+//git push 
+// npm s
---
+++ hello-world/assets/app/index.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+
+const HelloWorld = () => {
+    return(
+        <safeAreaView style={{
+            backgroundcolour: '#B99470'
+        }}>
+            <text> Hello world</Text>    
+       </view>
+    )
+}
+
+export defult HelloWorld 
